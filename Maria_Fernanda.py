09426f7c048a235b1fab50e5a2f313e9d1dc84b9
Maria Fernanda <mfer.martins25@gmail.com>
2026-07-03 20:24:25 -0300

    Add files via upload

---
+++ Maria_Fernanda.py
@@ -0,0 +1,18 @@
+coordenada_civic = (-23.5505,-46.6333)
+coordenada_uno = (-7.115,-34.863)
+veiculo_civic = {
+    "modelo":"civic",
+    "ano": 2024, 
+    "capacida_kg": 400.00,
+    "em operacao": True,
+    "ultima_coordenada":(-23.5505,-46.6333)
+    }
+veiculo_uno = {
+    "modelo":"uno",
+    "ano": 2013, 
+    "capacida_kg": 400.00,
+    "em operação": True,
+    "ultima_coordenada": (-7.115,-34.863)
+    }
+frota_aplicativo = [veiculo_civic,veiculo_uno]
+print(frota_aplicativo)
